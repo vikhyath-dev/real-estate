1f9f59fe29b6b8f1419a266cd9c28f71df54f55a
Tomasz Rudnicki <tomek198821@wp.pl>
2024-08-08 16:53:02 +0100

    installing react leaflet for maps and creating map with houses on it

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './layout.scss';
 import Navbar from '../../components/navbar/Navbar'
 import { Outlet } from 'react-router-dom';
 
--- src/pages/listPage/ListPage.jsx
+++ src/pages/listPage/ListPage.jsx
@@ -2,6 +2,7 @@ import './listPage.scss'
 import { listData } from '../../lib/dummydata'
 import Filter from '../../components/filter/Filter'
 import Card from '../../components/card/Card'
+import Map from '../../components/map/Map';
 
 export default function ListPage() {
 
@@ -17,7 +18,9 @@ export default function ListPage() {
           ))}
         </div>
       </div>
-      <div className="mapContainer">Map</div>
+      <div className="mapContainer">
+        <Map items={data} />
+      </div>
     </div>
   );
 }

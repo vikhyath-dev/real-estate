142b668a26ea3aae34297b11ccaca0f74aa4fe86
Tomasz Rudnicki <tomek198821@wp.pl>
2024-08-09 10:54:28 +0100

    adding picture slider, details and features to  singlePage

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './slider.scss'
 
-export default function Slider() {
+export default function Slider({ images }) {
+  const [imageIndex, setImageIndex] = useState(null);
+
+  const changeSlide = (direction) => {
+    if (direction === "left") {
+      if (imageIndex === 0) {
+        setImageIndex(images.length-1)
+      }
+      else {
+        setImageIndex(imageIndex-1)
+      }
+    } else {
+      if (imageIndex === images.length - 1) {
+        setImageIndex(0);
+      } else {
+        setImageIndex(imageIndex + 1);
+      }
+    }
+
+  }
+
   return (
-    <div>Slider</div>
-  )
+    <div className="slider">
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow" onClick={() => changeSlide("left")}>
+            <img src="icons8-arrow-100 (2).png" alt="" />
+          </div>
+          <div className="imgContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow" onClick={() => changeSlide("left")}>
+            <img src="/icons8-arrow-100.png" alt="" />
+          </div>
+          <div className="close" onClick={() => setImageIndex(null)}>
+            X
+          </div>
+        </div>
+      )}
+      <div className="bigImage">
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
+      </div>
+      <div className="smallImages">
+        {images.slice(1).map((image, index) => (
+          <img
+            src={image}
+            alt=""
+            key={index}
+            onClick={() => setImageIndex(index + 1)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }

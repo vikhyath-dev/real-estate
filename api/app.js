47a33615673083c1a20599b7ac2ea297a5cab605
Tomasz Rudnicki <tomek198821@wp.pl>
2024-08-14 16:20:35 +0100

    adding backend files to the project

---
+++ api/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import postRoute from "./routes/post.route.js";
+import authRoute from './routes/auth.route.js';
+import testRoute from './routes/test.route.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true }))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
+
+app.listen(8800, () => {
+    console.log("Server is running!");
+})
---
+++ api/routes/test.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  shouldBeLoggedIn,
+  shouldBeAdmin,
+} from "../controllers/test.controller.js";
+
+const router = express.Router();
+
+ router.get("/should-be-logged-in", shouldBeLoggedIn);
+ router.post("/should-be-admin", shouldBeAdmin);
+
+export default router;

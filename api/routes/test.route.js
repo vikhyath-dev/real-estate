45a7a91a93f547ffdc17acf8c9e269e3237b356f
Tomasz Rudnicki <tomek198821@wp.pl>
2024-08-14 17:22:39 +0100

    creating middleware for token verification

--- api/routes/test.route.js
+++ api/routes/test.route.js
@@ -3,10 +3,11 @@ import {
   shouldBeLoggedIn,
   shouldBeAdmin,
 } from "../controllers/test.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
- router.get("/should-be-logged-in", shouldBeLoggedIn);
- router.post("/should-be-admin", shouldBeAdmin);
+ router.get("/should-be-logged-in",verifyToken, shouldBeLoggedIn);
+ router.get("/should-be-admin", shouldBeAdmin);
 
 export default router;

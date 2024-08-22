f3339bcedcc0f8ad558f3244daab0fceb068bce1
Tomasz Rudnicki <tomek198821@wp.pl>
2024-08-22 15:22:40 +0100

    added saving post and profile posts to user controller and added saved post model to prisma

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,19 +1,34 @@
 import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
+
 
 export const getPosts = async (req, res) => {
-    try {
-        
-        const posts = await prisma.post.findMany()
+     const query = req.query;
 
-        res.status(200).json(posts)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({message:"Failed to get posts"})
-    }
+     try {
+       const posts = await prisma.post.findMany({
+         where: {
+           city: query.city || undefined,
+           type: query.type || undefined,
+           property: query.property || undefined,
+           bedroom: parseInt(query.bedroom) || undefined,
+           price: {
+             gte: parseInt(query.minPrice) || undefined,
+             lte: parseInt(query.maxPrice) || undefined,
+           },
+         },
+       });
+       setTimeout(() => {
+        res.status(200).json(posts);
+        }, 3000);
+     } catch (err) {
+       console.log(err);
+       res.status(500).json({ message: "Failed to get posts" });
+     }
 }
 
 export const getPost = async (req, res) => {
-    const id = req.params
+    const id = req.params.id
     try {
         const post = await prisma.post.findUnique({
           where: { id },
@@ -28,7 +43,32 @@ export const getPost = async (req, res) => {
           }
         })
         
-    res.status(200).json(post);
+      let userId;
+
+      const token = req.cookies?.token;
+
+      if (!token) {
+        userId = null;
+      } else {
+        jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+          if (err) {
+            userId = null;
+          } else {
+            userId = payload.id;
+          }
+        })
+      }
+
+      const saved = await prisma.savedPost.findUnique({
+        where: {
+          userId_postId: {
+            postId: id,
+            userId,
+          }
+        }
+      })
+      
+      res.status(200).json({ ...post, isSaved: saved ? true : false });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get post" });

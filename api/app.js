809af62f1dc3589aef2a0d45dc713f2a33e55735
Tomasz Rudnicki <tomek198821@wp.pl>
2024-08-22 16:11:07 +0100

    chat and message routes and controllers added

--- api/app.js
+++ api/app.js
@@ -5,6 +5,9 @@ import postRoute from "./routes/post.route.js";
 import authRoute from './routes/auth.route.js';
 import testRoute from './routes/test.route.js';
 import userRoute from './routes/user.route.js';
+import chatRoute from './routes/chat.route.js';
+import messageRoute from "./routes/message.route.js";
+
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -19,6 +22,8 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
+app.use("/api/chat", chatRoute);
+app.use("/api/message", messageRoute);
 
 
 app.listen(8800, () => {

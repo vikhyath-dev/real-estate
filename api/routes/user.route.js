c3cd6aef527f5e6908494ff4b09d5394c1b0cc27
Tomasz Rudnicki <tomek198821@wp.pl>
2024-08-26 17:29:09 +0100

    function for taking care of geting notification number for the messages added to user controller

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,7 +5,8 @@ import {
   getUsers,
   updateUser,
   savePost,
-  profilePosts
+  profilePosts,
+  getNotificationNumber
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -18,5 +19,6 @@ router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
 router.post("/save", verifyToken, savePost);
 router.get("/profilePosts", verifyToken, profilePosts);
+router.get("/notification", verifyToken, getNotificationNumber);
 
 export default router;

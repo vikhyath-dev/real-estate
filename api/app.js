2c7e26becde63e1b004e9d59e8464920b14cdb7e
vikhyath-dev <vikhyathvicky17@gmail.com>
2024-09-18 09:53:00 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true }))
+app.use(cors({origin: "https://real-estate-lyart-tau.vercel.app", credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 

7b61aabb1c6fc97b723d01413d0186b7f02eab6c
vikhyath-dev <vikhyathvicky17@gmail.com>
2024-09-18 09:24:12 +0530

    Update apiRequest.js

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://real-estate-api-beige.vercel.app/",
+  baseURL: "https://real-estate-jxs2.vercel.app/",
   withCredentials: true,
 });
 
